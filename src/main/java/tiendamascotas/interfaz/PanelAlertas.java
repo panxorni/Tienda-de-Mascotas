87ed7d060f07b32cfd011f1c568ce7cbfb7e0c77
Francisco Rubilar <franciscorubilarcea@gmail.com>
2026-06-27 14:16:43 -0400

    creo clase panel de alertas, falta metodo de dibujado

---
+++ src/main/java/tiendamascotas/interfaz/PanelAlertas.java
@@ -0,0 +1,11 @@
+package tiendamascotas.interfaz;
+
+import tiendamascotas.logica.patrones.observer.ObservadorMascotas;
+
+public class PanelAlertas implements ObservadorMascotas{
+
+    @Override
+    public void actualizarAlerta(String nombreMascota, String tipoAlerta, String mensaje){
+
+    }
+}
