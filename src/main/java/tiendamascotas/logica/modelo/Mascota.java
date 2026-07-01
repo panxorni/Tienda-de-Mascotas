1cef9e554d3062b895ec32149e0daec310f7ed2c
Gaeo24 <hr7xscan@gmail.com>
2026-06-30 22:34:07 -0400

    feat: metodos alimentar, jugar, etc. ahora modifican los atributos de la mascota al ejecutarse y metodo actualizarEstado cambia automaticamente el estado despues de una accion

--- src/main/java/tiendamascotas/logica/modelo/Mascota.java
+++ src/main/java/tiendamascotas/logica/modelo/Mascota.java
@@ -1,5 +1,7 @@
 package tiendamascotas.logica.modelo;
 import tiendamascotas.logica.patrones.observer.SujetoObservable;
+import tiendamascotas.logica.patrones.state.EstadoFeliz;
+import tiendamascotas.logica.patrones.state.EstadoHambriento;
 import tiendamascotas.logica.patrones.state.EstadoMascota;
 import tiendamascotas.logica.patrones.state.EstadoSaludable;
 
@@ -18,15 +20,30 @@ public abstract class Mascota extends SujetoObservable {
         this.salud = 100;     // Nace con salud max
         this.hambre = 0;      // Nace sin hambre
     }
-
+//Metodo para alimentar
     public void alimentar(){
-        estado.alimentar(this);
+        setHambre(hambre - 30);
+        setFelicidad(felicidad + 10);
     }
-
+//Metodo de juego, si no puede jugar notifica al observer, de lo contrario actualiza los atributos correspondientes
     public void jugar(){
-        estado.jugar(this);
+        if (!estado.puedeJugar()){
+            notificarObservadores(nombre, "Juego", nombre + "no quiere jugar ahora.");
+            return;
+        }
+        setFelicidad(felicidad + 15);
+        setHambre(hambre + 10);
     }
-
+//Metodo para limpiar el habitat de una mascota
+    public void limpiarHabitat(){
+        setSalud(salud + 10);
+        setFelicidad(felicidad + 5);
+    }
+//Metodo para dar medicina
+    public void darMedicina(){
+        setSalud(salud + 25);
+        setFelicidad(felicidad - 5);
+    }
     public void setEstado(EstadoMascota estado){
         this.estado=estado;
     }
@@ -43,21 +60,16 @@ public abstract class Mascota extends SujetoObservable {
         return felicidad;
     }
 
-
-
     public int getSalud(){
         return salud;
     }
-
-
 
     public int getHambre(){
         return hambre;
     }
 
-
     public void setFelicidad(int felicidad){
-        this.felicidad = felicidad;
+        this.felicidad = limitar(felicidad);
         // Detonante de aburrimiento/tristeza
         if (this.felicidad < 30) {
             notificarObservadores(this.nombre, "Aburrimiento", "¡" + this.nombre + " está muy triste y necesita jugar!");
@@ -65,7 +77,7 @@ public abstract class Mascota extends SujetoObservable {
     }
 
     public void setSalud(int salud){
-        this.salud = salud;
+        this.salud = limitar(salud);
         // Detonante de enfermedad
         if (this.salud < 40) {
             notificarObservadores(this.nombre, "Enfermedad", "¡La salud de " + this.nombre + " está bajando! Necesita medicina.");
@@ -73,10 +85,30 @@ public abstract class Mascota extends SujetoObservable {
     }
 
     public void setHambre(int hambre){
-        this.hambre = hambre;
+        this.hambre = limitar(hambre);
         // Detonante de hambre
         if (this.hambre > 80) {
             notificarObservadores(this.nombre, "Hambre", "¡" + this.nombre + " tiene mucha hambre!");
         }
+    }
+    //Metodo para actualizar el estado despues de cada accion
+    private void actualizarEstado(){
+        if(hambre>70){
+            estado = new EstadoHambriento();
+        } else if (felicidad > 75 && salud > 60) {
+            estado = new EstadoFeliz();
+        } else {
+            estado = new EstadoSaludable();
+        }
+    }
+    //Metodo para limitar valores de atributos entre 0 a 100
+    private int limitar(int valor) {
+        if (valor < 0){
+            return 0;
+        }
+        if (valor > 100){
+            return 100;
+        }
+        return valor;
     }
 }
