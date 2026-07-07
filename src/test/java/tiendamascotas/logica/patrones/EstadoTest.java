406a84a557b777e566ad54af846b14fbbfd36daf
Matías Vicente Pareja Escalona <matyparejaescalona@gmail.com>
2026-07-07 14:14:33 -0400

    Creo el test unitario de los state

---
+++ src/test/java/tiendamascotas/logica/patrones/EstadoTest.java
@@ -0,0 +1,120 @@
+package tiendamascotas.logica.patrones;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import tiendamascotas.logica.modelo.Mascota;
+import tiendamascotas.logica.modelo.mascotas.Perro;
+import tiendamascotas.logica.patrones.state.EstadoFeliz;
+import tiendamascotas.logica.patrones.state.EstadoHambriento;
+import tiendamascotas.logica.patrones.state.EstadoSaludable;
+
+import static org.junit.jupiter.api.Assertions.*;
+public class EstadoTest {
+    private Mascota mascota;
+
+    /**
+     * Antes de cada test se crea una mascota nueva.
+     */
+    @BeforeEach
+    void setUp() {
+        mascota= new Perro("Kota");
+    }
+
+    @Test
+    void estadoFelizDebeEntregarDatosCorrectos() {
+        EstadoFeliz estado= new EstadoFeliz();
+
+        assertEquals("Feliz" , estado.getNombre());
+        assertEquals("La mascota esta feliz", estado.getDescripcion());
+        assertTrue(estado.puedeJugar());
+        assertTrue(estado.puedeVenderse());
+    }
+
+    @Test
+    void estadoHambrientoDebeEntregarDatosCorrectos() {
+        EstadoHambriento estado= new EstadoHambriento();
+
+        assertEquals("Hambriento", estado.getNombre());
+        assertEquals("La mascota tiene hambre" , estado.getDescripcion());
+        assertFalse(estado.puedeJugar());
+        assertFalse(estado.puedeVenderse());
+    }
+
+    @Test
+    void estadoSaludableDebeEntregarDatosCorrectos() {
+        EstadoSaludable estado= new EstadoSaludable();
+
+        assertEquals("Saludable" , estado.getNombre());
+        assertEquals("La mascota esta en una buena condición", estado.getDescripcion());
+        assertTrue(estado.puedeJugar());
+        assertTrue(estado.puedeVenderse());
+    }
+
+    @Test
+    void alimentarMascotaHambrientaDebeQuitarElHambre() {
+        EstadoHambriento estado= new EstadoHambriento();
+
+        mascota.setHambre(80);
+        mascota.setFelicidad(50);
+        mascota.setEstado(estado);
+
+        estado.alimentar(mascota);
+
+        assertEquals(0 , mascota.getHambre());
+    }
+
+    @Test
+    void alimentarMascotaHambrientaDebeAumentarLaFelicidad() {
+        EstadoHambriento estado= new EstadoHambriento();
+
+        mascota.setHambre(80);
+        mascota.setFelicidad(50);
+        mascota.setEstado(estado);
+
+        estado.alimentar(mascota);
+
+        assertEquals(60, mascota.getFelicidad());
+    }
+
+    @Test
+    void alimentarMascotaHambrientaDebeCambiarEstadoAFeliz() {
+        EstadoHambriento estado= new EstadoHambriento();
+
+        mascota.setHambre(80);
+        mascota.setFelicidad(50);
+        mascota.setEstado(estado);
+
+        estado.alimentar(mascota);
+
+        assertInstanceOf(EstadoFeliz.class , mascota.getEstado());
+    }
+
+    @Test
+    void jugarConMascotaHambrientaDebeReducirLaFelicidad() {
+        EstadoHambriento estado= new EstadoHambriento();
+
+        mascota.setHambre(80);
+        mascota.setFelicidad(50);
+        mascota.setEstado(estado);
+
+        estado.jugar(mascota);
+
+        assertEquals(45 , mascota.getFelicidad());
+    }
+
+    @Test
+    void hambreAltaDebeCambiarElEstadoAHambriento() {
+        mascota.setHambre(80);
+
+        assertInstanceOf(EstadoHambriento.class , mascota.getEstado());
+    }
+
+    @Test
+    void mascotaConAltaFelicidadYBuenaSaludDebeEstarFeliz() {
+        mascota.setSalud(100);
+        mascota.setFelicidad(100);
+        mascota.setHambre(0);
+
+        assertInstanceOf(EstadoFeliz.class, mascota.getEstado());
+    }
+}
